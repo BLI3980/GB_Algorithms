8324e3b146273ff1f25760dd4931f9aa34bb5379
Boris Li <liba8936@protonmail.com>
2023-04-09 05:03:15 +0500

    Seminar04 complete

--- src/Seminars/Seminar04/Main.java
+++ src/Seminars/Seminar04/Main.java
@@ -1,5 +1,90 @@
 package Seminars.Seminar04;
 
+import java.util.*;
+import java.util.function.Consumer;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        // ==========================================================
+//        tree.add(5);
+//        tree.add(2);
+//        tree.add(10);
+//
+//        System.out.println(tree.contains(5)); // true
+//        System.out.println(tree.contains(2)); // true
+//        System.out.println(tree.contains(10)); // true
+//        System.out.println(tree.contains(-1)); // false
+//        System.out.println(tree.contains(4)); // false
+//
+//        System.out.println(tree.findFist()); // 2
+//        tree.remove(2);
+//        System.out.println(tree.findFist()); // 5
+//        tree.remove(5);
+//        System.out.println(tree.findFist()); // 10
+//        tree.remove(10);
+//        System.out.println(tree.findFist()); // Exception
+
+        // ==========================================================
+        tree.add(7);
+        tree.add(3);
+        tree.add(9);
+        tree.add(2);
+        tree.add(8);
+        tree.add(1);
+        tree.add(5);
+        tree.add(4);
+        tree.add(6);
+        // DFS depth-first-search - поиск в глубину
+        // BFS breadth-first-search - поиск в ширину
+        System.out.println(tree.dfs());
+        System.out.println(tree.bfs());
+
+        // ==========================================================
+        List<Integer> dfsConsumerOrderItems = new ArrayList<>();
+        final int[] sum = {0};
+//        tree.dfsConsumer(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) {
+//                dfsConsumerOrderItems.add(integer);
+//            }
+//        });
+
+        // ==== The above Consumer can also be wrapped into method reference
+        // ==== Meaning - each item of dfsConsumer method is added to dfsConsumerOrderItems list
+        tree.dfsConsumer(dfsConsumerOrderItems::add);
+        System.out.println(dfsConsumerOrderItems);
+
+        // ==== The Consumer can also be wrapped into Lambda
+        tree.dfsConsumer(integer -> sum[0] += integer);
+        System.out.println(Arrays.toString(sum));
+
+        // ==========================================================
+        List<Integer> bfsConsumerItems = new ArrayList<>();
+        tree.bfsConsumer(bfsConsumerItems::add);
+        System.out.println(bfsConsumerItems);
+
+        final int[] counter = {0};
+        tree.bfsConsumer(integer -> counter[0]++);
+        // tree -> tree; bfsConsumer -> method of tree traversal with Consumer method;
+        // in brackets -> each element of tree is processed by lambda
+        System.out.println(Arrays.toString(counter));
+
+    }
+
+    // ==========================================================
+    // HASH TABLE
+    // [null, null, null, null, null, null, null, null] = size 8
+    // element1: hash(element1) = 18 - example
+    // 18 % 8 = 2 - index for element1 in hash-table
+    // [null, null, [element1] - linked list, null, null, null, null, null]
+    // element2: hash(element2) = 23 - example
+    // 24 % 8 = 7 - index for element2 in hash-table
+    // [null, null, [element1] - linked list, null, null, null, null, [element2]]
+    // element(n): hash(element(n)) = also 18 - in rare occasion
+    // [null, null, [element1, element(n)], null, null, null, null, [element2]]
+
 
 }
--- src/Seminars/Seminar04/Tree.java
+++ src/Seminars/Seminar04/Tree.java
@@ -1,5 +1,8 @@
 package Seminars.Seminar04;
 
+import java.util.*;
+import java.util.function.Consumer;
+
 public class Tree {
     private class Node {
         private int value;
@@ -12,7 +15,7 @@ public class Tree {
 
     private Node root;
 
-    public void insert(int value) {
+    public void add(int value) {
         root = appendNode(root, value);
     }
 
@@ -44,6 +47,142 @@ public class Tree {
         }
         return current;
     }
+
+    public void remove(int value) {
+        root = removeNode(root, value);
+    }
+
+    private Node removeNode(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (current.value > value) {
+            current.left = removeNode(current.left, value);
+            return current;
+        } else if (current.value < value) {
+            current.right = removeNode(current.right, value);
+            return current;
+        }
+        // remove current node.
+        // case 1. no child nodes
+        if (current.left == null && current.right == null) {
+            return null;
+        }
+        // case2. only one child done
+        if (current.left == null) { // && current.right != null) {
+            return current.right;
+        }
+        if (current.right == null) { // && current.left != null) {
+            return current.left;
+        }
+        // case 3. both child nodes exist
+        // Need to find the smallest element from the right subtree of the node which is being deleted and
+        Node smallestNodeOnTheRight = findFirst(current.right);
+        // replace the node being deleted with found the smallest node from the right subtree. Then
+        current.value = smallestNodeOnTheRight.value;
+        // delete the found smallest element.
+        current.right = removeNode(current.right, smallestNodeOnTheRight.value);
+        // This way we know that new node is larger than any nodes from its left subtree and at the same
+        // time it is less than any nodes from its right subtree.
+        return current;
+    }
+
+    // Aux method to find the first smallest node from the right subtree
+    public int findFirst() {
+        if (root == null) {
+            throw new NoSuchElementException();
+        }
+        return findFirst(root).value;
+    }
+
+    private Node findFirst(Node current) {
+        if (current.left != null) {
+            return findFirst(current.left);
+        }
+        return current;
+    }
+
+    public List<Integer> dfs() {
+        List<Integer> result = new ArrayList<>();
+        dfs(root, result);
+        return result;
+    }
+
+    private void dfs(Node current, List<Integer> result) {
+        if (current != null) {
+            dfs(current.left, result);
+            result.add(current.value);
+            dfs(current.right, result);
+        }
+    }
+
+    public void dfsConsumer(Consumer<Integer> valueConsumer) {
+        List<Integer> result = new ArrayList<>();
+        dfsConsumer(root, valueConsumer);
+    }
+
+    private void dfsConsumer(Node current, Consumer<Integer> valueConsumer) {
+        if (current != null) {
+            dfsConsumer(current.left, valueConsumer);
+            valueConsumer.accept(current.value);
+            dfsConsumer(current.right, valueConsumer);
+        }
+    }
+
+    public List<Integer> bfs() {
+        List<Integer> result = new ArrayList<>();
+        bfs(result);
+        return result;
+    }
+
+    private void bfs(List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            result.add(node.value);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public void bfsConsumer(Consumer<Integer> valueConsumer) {
+        bfsConsumerInternal(valueConsumer);
+    }
+
+    private void bfsConsumerInternal(Consumer<Integer> valueConsumer) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            valueConsumer.accept(node.value);
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+
+
+
 
 
 }
